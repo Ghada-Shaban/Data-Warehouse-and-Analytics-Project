3fdaaf01474ae6efe9a563b5664ad03246bfad29
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-12-30 15:03:26 +0200

    Create cumulative_analysis.sql

---
+++ data analytics/scripts/cumulative_analysis.sql
@@ -0,0 +1,49 @@
+/*
+===============================================================================
+Cumulative Analysis
+===============================================================================
+Purpose:
+    - To calculate running totals or moving averages for key metrics.
+    - To track performance over time cumulatively.
+    - Useful for growth analysis or identifying long-term trends.
+
+SQL Functions Used:
+
+    - Window Functions: SUM() OVER(), AVG() OVER()
+===============================================================================
+*/
+
+-- Calculate the total sales per month (Running Total)
+ 
+SELECT 
+    order_date,
+    total_sales,
+    SUM(total_sales) OVER(  ORDER BY order_date) AS running_total
+FROM (
+SELECT 
+    DATETRUNC(month,order_date) AS order_date,
+    SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month,order_date)
+)t;
+
+-- 3-Month Moving Average Price per Year (Rolling Average)
+
+SELECT 
+    order_date,
+    total_sales,
+    SUM(total_sales) OVER(  ORDER BY order_date) AS running_total,
+    avg_price,
+    AVG(avg_price) OVER( PARTITION BY YEAR(order_date) ORDER BY order_date ROWS  BETWEEN 2 PRECEDING AND CURRENT ROW ) AS moving_avg_price_3months
+FROM (
+SELECT 
+    DATETRUNC(month,order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    AVG(price) AS avg_price
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month,order_date)
+)t
+
+
