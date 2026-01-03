6ebeb4e95ec0e655179a7f0225adcbca35d3d8eb
Ghada-Shaban <ghadaashaban2003@gmail.com>
2026-01-03 19:02:17 +0200

    Update 13- products_report.sql

--- data analytics/scripts/13- products_report.sql
+++ data analytics/scripts/13- products_report.sql
@@ -94,7 +94,7 @@ SELECT
 	    avg_selling_price,
 
         CASE WHEN total_sales = 0 THEN 0
-	         ELSE total_sales / total_orders
+	         ELSE CAST(total_sales AS decimal(10,2)) / NULLIF(total_orders,0)
         END AS avg_order_revenue,
 
         CASE WHEN lifespan = 0 THEN total_sales
